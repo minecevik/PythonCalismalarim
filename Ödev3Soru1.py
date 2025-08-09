878e33abf80802c9e5e1e37d47a9b706f1897e8d
minecevik <minecevik09@gmail.com>
2025-08-09 12:58:41 +0300

    Add files via upload

---
+++ Ödev3Soru1.py
@@ -0,0 +1,36 @@
+'''
+
+Sru 1: İkili Arama (Binary Search)
+Artan sırada sıralanmış bir tam sayı dizisinde, verilen bir sayının var olup olmadığını kontrol eden bir ikili arama algoritması uygulayınız. 
+Algoritmanızın zaman karmaşıklığını belirtiniz ve neden ikili aramanın doğrusal aramadan daha verimli olduğunu açıklayınız.
+
+'''
+
+import array
+sayilar=array.array('i', [2,4,6,8,10,12])
+
+def binary_search(dizi, hedef):
+    baslangic= 0
+    bitis= len(dizi)-1
+    
+    while baslangic<=bitis:
+        orta= (baslangic+bitis)//2
+        
+        if dizi[orta]==hedef:
+            return orta
+        
+        elif dizi[orta]<hedef:
+            baslangic= orta+1
+            
+        else:
+            bitis=orta-1
+            
+    return-1
+    
+hedef=5
+sonuc= binary_search(sayilar, hedef)
+
+if sonuc!=-1:
+    print(f'{hedef} sayisi {sonuc}. indekste bulundu.')
+else:
+    print(f'{hedef} sayisi dizide yok. ')
