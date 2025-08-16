1b376c04a2987bca490b1711fef6b682456eced3
minecevik <minecevik09@gmail.com>
2025-08-16 20:19:01 +0300

    Update Ödev1soru2.py

--- Ödev1soru2.py
+++ Ödev1soru2.py
@@ -1,3 +1,14 @@
+'''
+Problem 2: Zıplayan Top
+Belirli bir yükseklikten (h0) serbest bırakılan bir topun zıplama hareketini simüle et. Top, her zıpladığında enerjisinin bir kısmını kaybeder ve bir önceki zıplamasındaki yüksekliğin belirli bir oranı (katsayi, örn: 0.7) kadar yükselebilir.
+İstenenler:
+1. Kullanıcıdan başlangıç yüksekliğini (h0) ve enerji kayıp katsayısını (katsayi) al.
+2. Toplamda 5 zıplama için bir for döngüsü kur.
+3. Her zıplama için ulaşılan maksimum yüksekliği hesapla ve ekrana yazdır.
+4. Eğer bir zıplamada ulaşılan yükseklik 1 santimetreden (0.01 metre) daha az ise,
+döngüyü erken sonlandır ve "Top durdu." mesajı ver. (break komutunu araştırabilirsin).
+'''
+
 #Veriler
 h_0= float(input("Başlangıç yüksekliği:"))
 katsayi= float(input("Enerji kaybı katsayısı:"))
