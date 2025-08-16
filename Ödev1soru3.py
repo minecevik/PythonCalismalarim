8a21a8822af870fb42aef63d52cbcbbfda329564
minecevik <minecevik09@gmail.com>
2025-08-16 20:20:31 +0300

    Update Ödev1soru3.py

--- Ödev1soru3.py
+++ Ödev1soru3.py
@@ -1,3 +1,16 @@
+'''
+Problem 3: Asal Sayılar
+Matematik ve kriptografi gibi alanlarda temel olan asal sayıları bulan bir program yaz.
+İstenenler:
+1. Program, 2'den 100'e kadar olan bütün sayıları kontrol etmeli (dış for döngüsü).
+2. Her bir sayının asal olup olmadığını kontrol etmek için, o sayıyı 2'den başlayıp kendisine kadar
+olan sayılara bölmeye çalış (iç for döngüsü).
+3. Eğer bir sayı, kendisinden küçük (ve 1'den büyük) hiçbir sayıya tam bölünmüyorsa, o sayı asaldır.
+4. Bulduğun tüm asal sayıları ekrana yazdır.
+5. Ekstra Zorluk: Asal sayıları bulduktan sonra, aralarındaki fark 2 olan "ikiz asalları" (örn: (3, 5), (5,
+7), (11, 13)) bulup onları da ayrıca listele.
+'''
+
 #Boş bir liste oluşturuyorum
 asal_sayilar= []
 
